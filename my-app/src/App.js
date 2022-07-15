435a1844401fcc0bd9afda7dbc409d84358b8143
Smit <ptlsmit.b@gmail.com>
2022-07-15 10:19:45 -0700

    final commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,35 +1,47 @@
-import React, { useState } from 'react'
-import data from './data'
+import React, { useState } from "react";
+import data from "./data";
 
 const App = () => {
-
-  const [count,setCount] = useState(0)
+  const [count, setCount] = useState(0);
   const [text, setText] = useState([]);
-  
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    console.log("Hello World")
-  }
-  
-  return (
-    <section className='section-center'>
-      <h3>Tire of Boring Lorem ipsum ?</h3>
-      <form className='lorem-form'onSubmit={handleSubmit}>
-        <label htmlFor='amount'>
-          Paragraphs:
-        </label>
-        <input type="number"
-        name='amount'
-        id='amount'
-        value={count}
-        onChange={(e)=>setCount(e.target.value)}
-        />
-        <button type='submit'
-        className='btn'> Generate</button>
-      </form>
-    </section>
-    
-  )
-}
+  const[msg,setMsg] = useState([])
 
-export default App
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    if (count <= 0) {
+      amount = 1
+     
+    }
+    if (count > 8) {
+      amount = 8;
+    }
+    setText(data.slice(0, amount));
+  };
+  return (
+    <section className="section-center">
+      <h3>Tire of Boring Lorem ipsum ?</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">Paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type="submit" className="btn">
+          {" "}
+          Generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
+      </article>
+    </section>
+  );
+};
+
+export default App;
